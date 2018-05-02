75d2c5fc8f8e79b5ddaaf94a88388beae60763f5
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-05-02 20:53:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file
-function findMathching(drivers, string) {
+function findMatching(drivers, string) {
   drivers.filter(function (name) {return name.toLowerCase() === string.toLowerCase()});
 }
